27596359c94724ed7eeced5fb3f64563478be48f
GetCodePanda <hicodepanda@gmail.com>
2017-09-20 17:47:29 +0530

    simple sidebar container initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  // menus list ...
+    menus = [
+      { viewContent: 'Home', routerLinkPath: 'home' },
+      { viewContent: 'About', routerLinkPath: 'about' },
+      { viewContent: 'Service', routerLinkPath: 'service' },
+      { viewContent: 'Contact', routerLinkPath: 'contact' }
+    ];
+
     isOpen;
     toggleNav() {
       return this.isOpen = !this.isOpen;
     }
+
 
     ngOnInit() {
     }
